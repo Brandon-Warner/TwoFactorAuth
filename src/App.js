b6bc54d5a2ea5d562b307c7c67704056726e00a3
Brandon Warner <brandonswarner5@gmail.com>
2021-12-14 13:29:15 -0800

    installed styled components, created the MVP UI for the form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import styled from 'styled-components';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
+        <Container>
+            <Form type='submit'>
+                <Input type='text' />
+                <Input type='text' />
+                <Input type='text' />
+                <Input type='text' />
+
+                <Button>submit</Button>
+            </Form>
+        </Container>
     );
 };
+
+const Container = styled.div`
+    padding: 0;
+    margin: 0;
+    text-align: center;
+`;
+
+const Form = styled.form`
+    margin: 15% auto;
+    width: 75%;
+    height: 200px;
+    item-align: center;
+    display: grid;
+    gap: 1em;
+    grid-template-columns: repeat(4, 25%);
+    grid-template-rows: 80% 20%;
+`;
+
+const Input = styled.input`
+    height: 100px;
+    width: 100px;
+    margin: 0 auto;
+    font-size: 40px;
+    text-align: center;
+`;
+
+const Button = styled.button`
+    display: inline-block;
+    grid-column: 4 / span 1;
+    width: 50%;
+    margin: 0 auto;
+    text-align: center;
+`;
 
 export default App;
